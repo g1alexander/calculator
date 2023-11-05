a4fe159f38e08ee99ef008449e7104775af9263e
g1alexander <alexander.granados.dev@gmail.com>
2023-11-05 09:52:43 -0500

    feat: operations

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -12,13 +12,134 @@ class ViewController: UIViewController {
     @IBOutlet weak var value: UILabel!
     
     var valueOperation = 0.0
+    var firstValue = 0.0
+    var signOperation = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         value.text = "0"
     }
-
+    
+    @IBAction func divided(_ sender: UIButton) {
+        DispatchQueue.main.async {
+            self.value.text = "0"
+        }
+        firstValue = valueOperation
+        signOperation = "/"
+//        sender.layer.backgroundColor = UIColor.white.cgColor
+//        sender.setTitleColor(UIColor(red: 254, green: 159, blue: 10, alpha: 1.0), for: .normal)
+    }
+    
+    @IBAction func Multiplication(_ sender: UIButton) {
+        DispatchQueue.main.async {
+            self.value.text = "0"
+        }
+        firstValue = valueOperation
+        signOperation = "*"
+    }
+    
+    @IBAction func minus(_ sender: UIButton) {
+        DispatchQueue.main.async {
+            self.value.text = "0"
+        }
+        firstValue = valueOperation
+        signOperation = "-"
+    }
+    
+    @IBAction func sum(_ sender: UIButton) {
+        DispatchQueue.main.async {
+            self.value.text = "0"
+        }
+        firstValue = valueOperation
+        signOperation = "+"
+    }
+    
+    @IBAction func operation(_ sender: UIButton) {
+        switch signOperation {
+        case "/":
+            let newValue = firstValue / valueOperation
+            DispatchQueue.main.async { [self] in
+                
+                var convertString = ""
+                
+                if newValue.truncatingRemainder(dividingBy: 1) != 0 {
+                     convertString = String(newValue).replacingOccurrences(of: ".", with: ",")
+                } else {
+                    convertString = "\(Int(newValue))"
+                }
+                
+                value.text = "\(convertString)"
+            }
+        case "*":
+            let newValue = firstValue * valueOperation
+            DispatchQueue.main.async { [self] in
+                
+                var convertString = ""
+                
+                if newValue.truncatingRemainder(dividingBy: 1) != 0 {
+                     convertString = String(newValue).replacingOccurrences(of: ".", with: ",")
+                } else {
+                    convertString = "\(Int(newValue))"
+                }
+                
+                value.text = "\(convertString)"
+            }
+        case "-":
+            let newValue = firstValue - valueOperation
+            DispatchQueue.main.async { [self] in
+                
+                var convertString = ""
+                
+                if newValue.truncatingRemainder(dividingBy: 1) != 0 {
+                     convertString = String(newValue).replacingOccurrences(of: ".", with: ",")
+                } else {
+                    convertString = "\(Int(newValue))"
+                }
+                
+                value.text = "\(convertString)"
+            }
+        case "+":
+            let newValue = firstValue + valueOperation
+            DispatchQueue.main.async { [self] in
+                
+                var convertString = ""
+                
+                if newValue.truncatingRemainder(dividingBy: 1) != 0 {
+                     convertString = String(newValue).replacingOccurrences(of: ".", with: ",")
+                } else {
+                    convertString = "\(Int(newValue))"
+                }
+                
+                value.text = "\(convertString)"
+            }
+        default:
+            print("Opción no reconocida")
+        }
+    }
+    
+    @IBAction func percentage(_ sender: UIButton) {
+        let newValue = valueOperation / 100
+        
+        DispatchQueue.main.async { [self] in
+            
+            var convertString = ""
+            
+            if newValue.truncatingRemainder(dividingBy: 1) != 0 {
+                 convertString = String(newValue).replacingOccurrences(of: ".", with: ",")
+            } else {
+                convertString = "\(Int(newValue))"
+            }
+            
+            value.text = "\(convertString)"
+        }
+        
+        valueOperation = newValue
+        
+        print(valueOperation)
+        print(newValue)
+    }
+    
     @IBAction func resetValue(_ sender: UIButton) {
         value.text = "0"
         valueOperation = 0.0
